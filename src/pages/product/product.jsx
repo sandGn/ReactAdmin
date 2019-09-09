17f456db3464397a3843b64289518cdac33c776e
Sand <sand.mail@foxmail.com>
2019-09-09 15:44:50 +0800

    product add update

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import ProductHome from './home'
+import ProductAddUpdate from './add-update'
+import ProductDetail from './detail'
+import './product.less'
 /*
 商品
  */
 export default class Product extends Component {
     render() {
         return (
-            <div></div>
+            <Switch>
+                <Route path='/product' component={ProductHome} exact /> {/*路径完全匹配*/}
+                <Route path='/product/addupdate' component={ProductAddUpdate} />
+                <Route path='/product/detail' component={ProductDetail} />
+                <Redirect to='/product' />
+            </Switch>
         )
     }
 }
